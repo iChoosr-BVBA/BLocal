d32fe22384fb33375f038d73446704286c5c38b7
Sam Meurs <sam@ichoosr.com>
2014-11-10 17:30:30 +0100

    #1573652 view history

---
+++ BLocal.Web.Manager.MVC3/Controllers/HistoryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using BLocal.Web.Manager.Business;
+using BLocal.Web.Manager.Context;
+using BLocal.Web.Manager.Extensions;
+
+namespace BLocal.Web.Manager.Controllers
+{
+    [Authenticate]
+    public class HistoryController : Controller
+    {
+        private const string HistoryProviderGroupName = "historyProviderGroup";
+
+        public ProviderGroupFactory ProviderGroupFactory { get; set; }
+
+        public HistoryController()
+        {
+            ProviderGroupFactory = new ProviderGroupFactory();
+        }
+
+        public ActionResult Index(String providerConfigName)
+        {
+            var localization = Session.Get<ProviderGroup>(HistoryProviderGroupName);
+            if (localization == null || localization.Name != providerConfigName)
+                Session.Set(HistoryProviderGroupName, localization = ProviderGroupFactory.CreateProviderGroup(providerConfigName));
+
+            var allValues = localization.ValueManager.GetAllValuesQualified();
+            localization.HistoryManager.AdjustHistory(allValues, Session.Get<String>("author"));
+            var history = localization.HistoryManager.ProvideHistory()
+                .OrderByDescending(h => h.LatestEntry().DateTime)
+                .ToList();
+
+            return View(history);
+        }
+    }
+}
