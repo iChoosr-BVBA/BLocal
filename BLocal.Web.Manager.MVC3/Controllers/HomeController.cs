7e7eddf88bd81b4735d1e1b464b11e5183e75c7a
Damian <damian@ichoosr.com>
2017-05-18 15:44:08 +0200

    google sign in added

--- BLocal.Web.Manager.MVC3/Controllers/HomeController.cs
+++ BLocal.Web.Manager.MVC3/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Configuration;
+using System.Net;
+using System.Linq;
 using System.Web.Mvc;
 using BLocal.Web.Manager.Context;
+using BLocal.Web.Manager.Models.GoogleAuthorisation;
+using Newtonsoft.Json;
 
 namespace BLocal.Web.Manager.Controllers
 {
@@ -25,6 +29,40 @@ namespace BLocal.Web.Manager.Controllers
                 Session["auth"] = DateTime.Now;
             Session["author"] = username;
             return RedirectToAction("Overview");
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GoogleLogin(string token)
+        {
+            // specify allowed domains
+            string[] allowedDomains = new string[1] { "ichoosr.com" };
+            // decode google token_id    
+            GoogleToken googleToken = DecodeToken(token);
+            bool data = false;
+            // check if domain is allowed to enter
+            if (googleToken.EmailVerified == "true" && allowedDomains.Contains(googleToken.Email.Split('@')[1]))
+            {
+                Session["auth"] = DateTime.Now;
+                Session["author"] = googleToken.Email;
+                data = true;
+            }
+            // return result of the action
+            return new JsonResult() { Data = data };
+        }
+
+        private GoogleToken DecodeToken(string token)
+        {
+            string url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token={token_id}";
+            WebRequest httpRequest = WebRequest.Create(url.Replace("{token_id}", token));
+            httpRequest.Method = "GET";
+            var response = httpRequest.GetResponse();
+            string result = null;
+            using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+            {
+                result = reader.ReadToEnd();
+            }
+            var googleToken = JsonConvert.DeserializeObject<GoogleToken>(result);
+            return googleToken;
         }
     }
 }
